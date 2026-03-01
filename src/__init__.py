cf53360ef693445f5405ed1ea0247bdbfd5294bf
A.Trafimovich <deazushka@gmail.com>
2026-03-01 12:54:20 +0200

    Cleanup: Remove duplicated files from remote

--- src/__init__.py
+++
@@ -1,6 +0,0 @@
-from .database import Database
-from .handlers import Handlers
-from .constants import State, Callback, t
-from .keyboards import main_menu_keyboard
-
-__all__ = ["Database", "Handlers", "State", "Callback", "t", "main_menu_keyboard"]
